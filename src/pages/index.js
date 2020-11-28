a582190a5f1baa4cfb904aa4b4373310a4c88161
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-28 10:11:12 -0500

    added hero to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Hero from "../components/hero"
 
 const IndexPage = () => {
-  return <Layout>I am a good-looking page, right?</Layout>
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  )
 }
 
 export default IndexPage
